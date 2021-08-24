171489bda60869a00d535dbd6c4e88ef7c9d514f
wanglonggang_10804185 <wanglonggang@shinhofood.com>
2021-08-24 15:55:51 +0800

    update: 升级包版本

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,4 +1,11 @@
-export * from './@shinhotech/sh-ui'
+/*
+ * @Descripttion: 引入
+ * @Author: 18041085
+ * @Date: 2021-08-24 10:24:53
+ * @LastEditors: 18041085
+ * @LastEditTime: 2021-08-24 15:18:41
+ */
+export * from './element-ui';
 
-import * as ElementUI from './@shinhotech/sh-ui'
-export default ElementUI
+import * as ElementUI from './element-ui';
+export default ElementUI;
